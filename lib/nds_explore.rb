c605248406849d2e88b0d3f1255a5a7d3b4354a2
Carla D Sahagun <daniela.sahagun@hotmail.com>
2020-02-11 19:39:10 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -11,7 +11,7 @@ end
 def print_first_directors_movie_titles
   row_index = 0
   while row_index <= 6 do
-  pp directors_database[0][:movies][row_index][:title]
+  puts directors_database[0][:movies][row_index][:title]
   row_index += 1
   end
 end
