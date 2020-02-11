c4c801ca61376f661fef253818e90487150a85eb
Carla D Sahagun <daniela.sahagun@hotmail.com>
2020-02-11 19:51:13 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -12,8 +12,7 @@ def print_first_directors_movie_titles
   row_index = 0
   while row_index <= directors_database.length do
     if directors_database[row_index] == "Stephen Spielberg"
-
-      p directors_database[row_index]
+      pp directors_database[row_index]
     end
   row_index += 1
   end
