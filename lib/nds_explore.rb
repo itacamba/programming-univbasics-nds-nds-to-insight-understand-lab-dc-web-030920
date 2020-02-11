09d4ef9d3ddc60bf5a1d67e581f239be2a047a79
Carla D Sahagun <daniela.sahagun@hotmail.com>
2020-02-11 19:21:43 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -10,8 +10,12 @@ end
 
 def print_first_directors_movie_titles
   row_index = 0
+  my_first_director = {}
   while row_index < directors_database.length do
-    puts directors_database[row_index]
+    if row_index[:name] == "Stephen Spielberg"
+      my_first_director << row_index[0]
+    end
+    directors_database[row_index]
     row_index += 1
   end
 
